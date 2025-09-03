a393cf96c3b904fc49f9738a2b052438b905354f
tvthanh2k3 <tvthanh1201@gmail.com>
2025-09-03 15:52:20 +0700

    feat: connect database sql server

---
+++ ClothingStore.Database/Contexts/AppModuleBuilder.cs
@@ -0,0 +1,24 @@
+﻿using ClothingStore.Entity.Roles;
+using ClothingStore.Entity.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClothingStore.Database.Contexts
+{
+	public class AppModuleBuilder
+	{
+		public static void RegisterModule(ModelBuilder builder)
+		{
+			// Custom User
+			builder.Entity<User>()
+				.HasIndex(u => u.NormalizedUserName)
+				.HasDatabaseName("UserNameIndex")
+				.IsUnique(false); // Loại bỏ Unique Constraint
+
+			// Custom Role
+			builder.Entity<Role>()
+				.HasIndex(r => r.NormalizedName)
+				.HasDatabaseName("RoleNameIndex")
+				.IsUnique(false); // Loại bỏ Unique Constraint
+		}
+	}
+}
---
+++ ClothingStore.Entity/BaseEntities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace ClothingStore.Entity.BaseEntities
+{
+	public class BaseEntity
+	{
+		public int Id { get; set; }
+		public DateTime CreatedTime { get; set; }
+		public string? CreatedBy { get; set; }
+		public DateTime ModifiedTime { get; set; }
+		public string? ModifiedBy { get; set; }
+	}
+}
